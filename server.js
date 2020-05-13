f7f20d9c178a225c1655b3f74b613663299d3ab6
Dustin <df1837@gmail.com>
2020-05-13 14:59:53 -0700

    Added Heroku deployment settings.

--- server.js
+++ server.js
@@ -29,4 +29,12 @@ app.get('/api/getQuestion', (req, res) => {
     });
 });
 
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, 'client/build')));
+
+    app.get('*', function(req, res) {
+        res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+    });
+}
+
 app.listen(port, () => console.log(`Listening on port ${port}`));
